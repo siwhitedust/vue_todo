1e0f40d9cc74d8ac7993b3dde856cc649a8ad478
whitedust <whitedust222@gmail.com>
2026-07-14 15:18:39 +0700

    ganti base di vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/vue-todolist/',
+  base: '/vue_todo/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
